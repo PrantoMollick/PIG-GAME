137c1d268dda62ce1139a2ee7d9bd720a14843d6
pranto mollick <prantocse01@gmail.com>
2023-01-06 17:37:59 +0600

    rolling the dice

--- script.js
+++ script.js
@@ -2,6 +2,8 @@
 
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
+const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('current--1');
 const diceEl = document.querySelector('.dice');
 const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
@@ -10,6 +12,8 @@ const btnHold = document.querySelector('.btn--hold');
 score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
+
+let currentScore = 0;
 
 // Rolling dice functionality
 btnRoll.addEventListener('click', () => {
@@ -20,4 +24,12 @@ btnRoll.addEventListener('click', () => {
   //2. display the dice
   diceEl.classList.remove('hidden');
   diceEl.src = `img/dice-${dice}.png`;
+
+  if (dice !== 1) {
+    //add dice to current score
+    currentScore += dice;
+    current0El.textContent = currentScore; //change later
+  } else {
+    //switch to next player
+  }
 });
